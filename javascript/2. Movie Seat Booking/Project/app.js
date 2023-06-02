002a2f99da23633f8e3d2bc0fa6a2902b8c6139b
Vivek Kumar <VK285090@GMAIL.COM>
2023-06-02 23:54:00 +0530

    Initial commit

---
+++ javascript/2. Movie Seat Booking/Project/app.js
@@ -0,0 +1,35 @@
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+let ticketPrice = +movieSelect.value;
+
+//Movie select event
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  updatedSelectedCount();
+});
+
+//seat click event
+function updatedSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+    updatedSelectedCount();
+  }
+});
