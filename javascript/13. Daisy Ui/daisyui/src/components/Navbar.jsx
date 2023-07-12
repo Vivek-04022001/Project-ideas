b715f6a23e3dced9d8b0e493fed7953bf3ae14e1
Vivek Kumar <VK285090@GMAIL.COM>
2023-07-12 16:42:30 +0530

    add

---
+++ javascript/13. Daisy Ui/daisyui/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+
+
+const Navbar = () => {
+  return (
+
+<div className="navbar bg-red-300 container w-100 h-[150px] ">
+  <div className="flex-1">
+    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Link</a></li>
+      <li>
+        <details>
+          <summary>
+            Parent
+          </summary>
+          <ul className="p-2 bg-base-100">
+            <li><a>Link 1</a></li>
+            <li><a>Link 2</a></li>
+          </ul>
+        </details>
+      </li>
+    </ul>
+  </div>
+</div>
+
+
+  )
+}
+
+export default Navbar
